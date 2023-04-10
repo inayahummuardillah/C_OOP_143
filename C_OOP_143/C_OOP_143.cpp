c10a70007186d1c5a6712f8120c7e7f45b19183f
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-04-10 10:24:03 +0630

    class Mahasiswa beserta variable dan prosedur, dengan akses modifier public

--- C_OOP_143/C_OOP_143.cpp
+++ C_OOP_143/C_OOP_143.cpp
@@ -10,25 +10,3 @@ public: void cetak() {
 	cout << "\nNama = " << nama;
     }
 };
-
-class Matakuliah {
-private :
-	string kode;
-	string namaM;
-	int sks;
-public :
-	void input() {
-		cout << "Masukan Kode Matakuliah : ";
-		cin >> kode;
-		cout << "Masukan Nama Matakuliah : ";
-		cin >> namaM;
-		cout << "Jumlah SKS : ";
-		cin >> sks;
-		cout << "Masukan NIM : ";
-	}
-	void tampil() {
-		cout << "Kode Matakuliah : " << kode;
-		cout << "Nama Matakuliah : " << namaM;
-		cout << "SKS : " << sks;
-	}
-};
