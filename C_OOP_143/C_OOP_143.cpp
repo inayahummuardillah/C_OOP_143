ed34c8d8d75c58e9129ebc36a9c2d7ffbc101a24
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-04-10 10:16:08 +0630

    Add project files.

---
+++ C_OOP_143/C_OOP_143.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public :
+	string nim;
+	string nama;
+public: void cetak() {
+	cout << "NIM = " << nim;
+	cout << "\nNama = " << nama;
+    }
+};
+
+class Matakuliah {
+private :
+	string kode;
+	string namaM;
+	int sks;
+public :
+	void input() {
+		cout << "Masukan Kode Matakuliah : ";
+		cin >> kode;
+		cout << "Masukan Nama Matakuliah : ";
+		cin >> namaM;
+		cout << "Jumlah SKS : ";
+		cin >> sks;
+		cout << "Masukan NIM : ";
+	}
+	void tampil() {
+		cout << "Kode Matakuliah : " << kode;
+		cout << "Nama Matakuliah : " << namaM;
+		cout << "SKS : " << sks;
+	}
+};
