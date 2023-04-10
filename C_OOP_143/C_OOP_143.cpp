99341b2f72320fc9f6a39d66eb36bb853ed722c8
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-04-10 12:49:40 +0630

    class Matakuliah beserta variable dan prosedur dengan akses modifier private and public

--- C_OOP_143/C_OOP_143.cpp
+++ C_OOP_143/C_OOP_143.cpp
@@ -17,4 +17,29 @@ private:
 	string kode;
 	string namaM;
 	int sks;
+public:
+	void input() {
+		cout << "\nMasukan Kode Matakuliah : ";
+		cin >> kode;
+		cout << "Masukan Nama Matakuliah : ";
+		cin >> namaM;
+		cout << "Jumlah SKS : ";
+		cin >> sks;
+		cout << "Masukan NIM : ";
+	}
+};
+
+int main()
+{
+	Mahasiswa mhs1;
+	Matakuliah mk;
+
+	cout << "Masukan NIM :";
+	cin >> mhs1.nim;
+	cout << "Masukan nama : ";
+	cin >> mhs1.nama;
+	mhs1.cetak();
+
+	mk.input();
+	mk.tampil();
 }
